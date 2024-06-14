277fbca2f096762ab423159ad3396036a5b0463f
films999movie <anupsunuwar3@gmail.com>
2024-06-15 03:04:03 +0530

    Update Curse-Of-The-Nun-hide.js

--- Curse-Of-The-Nun-hide.js
+++ Curse-Of-The-Nun-hide.js
@@ -1,1 +1,22 @@
+function loadJSAtOnload() {
+var scripts = [
 
+
+"/Horror/Curse-Of-The-Nun.js",
+
+
+];
+
+for (var i = 0; i < scripts.length; i++) {
+    console.log('Loading script ' + scripts[i]);
+    var scriptType = document.createElement('script');
+    scriptType.type = 'text/javascript';
+    scriptType.src = scripts[i];
+    document.body.appendChild(scriptType);  
+}
+};
+if (window.addEventListener)
+    addEventListener('load', loadJSAtOnload, false);
+else if (window.attachEvent)
+    window.attachEvent('onload', loadJSAtOnload);
+else window.onload = loadJSAtOnload;
